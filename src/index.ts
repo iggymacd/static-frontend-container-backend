a6aa1dde7b997502f1c889c40c04f5c76d5c6674
Mike Nomitch <mnomitch@cloudflare.com>
2025-06-23 17:06:53 +0100

    Updates to latest package

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Container, loadBalance } from "@cloudflare/containers";
+import { Container, getRandom } from "@cloudflare/containers";
 
 export class Backend extends Container {
   defaultPort = 8080; // pass requests to port 8080 in the container
@@ -16,8 +16,9 @@ export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
     if (url.pathname.startsWith("/api")) {
-      // note: "loadBalance" to be replaced with latency-aware routing in the near future
-      const containerInstance = await loadBalance(env.BACKEND, INSTANCE_COUNT)
+      // note: "getRandom" to be replaced with latency-aware routing in the near future
+      // this is a temporary helper
+      const containerInstance = await getRandom(env.BACKEND, INSTANCE_COUNT)
       return containerInstance.fetch(request);
     }
 
